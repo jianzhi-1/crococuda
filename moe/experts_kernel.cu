35a36fda46ce999633e391ae609fc2cf38e25dcd
jianzhi-1 <jianzhiw00@gmail.com>
2026-06-19 14:18:29 -0400

    added tests

--- moe/experts_kernel.cu
+++ moe/experts_kernel.cu
@@ -4,7 +4,9 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-__global__ void count(
+#include "experts_kernel.cuh"
+
+__global__ void count_kernel(
     const int* expert_idx, // [B, K]
     int* counts, // [N]
     int total // BK
@@ -15,7 +17,7 @@ __global__ void count(
     }
 }
 
-__global__ void prefix_sum(
+__global__ void prefix_sum_kernel(
     const int* counts, // [N]
     uint32_t* expert_offsets, // [N + 1]
     int N
@@ -26,26 +28,5 @@ __global__ void prefix_sum(
         for (int i = 0; i < N; i++){
             expert_offsets[i + 1] = expert_offsets[i] + counts[i];
         }
-    }
-}
-
-template <typename T>
-__global__ void scatter(
-    const int* expert_idx, // [B, K]
-    const T* x, // [B, D]
-    uint32_t* write_cursor, // [N]
-    int* sorted_token_idx, // [BK]
-    T* sorted_x, // [BK, D]
-    int B, int D, int K, int N
-){
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx < B*K){
-        int token_idx = idx / K;
-        int expert_id = expert_idx[idx];
-        int write_idx = atomicAdd(&write_cursor[expert_id], 1);
-        for (int d = 0; d < D; d++){
-            sorted_x[write_idx * D + d] = x[token_idx * D + d];
-        }
-        sorted_token_idx[write_idx] = token_idx;
     }
 }
