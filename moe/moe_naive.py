4d8b9f5d9250832b41b3f62fb075f008e243c2d9
jianzhi-1 <jianzhiw00@gmail.com>
2026-06-20 06:55:00 -0400

    standardize abbreviations

--- moe/moe_naive.py
+++ moe/moe_naive.py
@@ -3,44 +3,44 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class MoELayer(nn.Module):
-    def __init__(self, n_experts: int, k: int, d_model: int, epsilon: float = 1e-5, alpha: float = 1e-2, activation_cls: type[nn.Module] = nn.SiLU):
+    def __init__(self, N: int, K: int, D: int, epsilon: float = 1e-5, alpha: float = 1e-2, activation_cls: type[nn.Module] = nn.SiLU):
         super().__init__()
-        self.n_experts = n_experts
-        self.d_model = d_model
-        self.E = nn.Linear(d_model, n_experts * d_model)
-        self.experts = nn.ModuleList([nn.Linear(d_model, d_model) for _ in range(n_experts)])
+        self.N = N
+        self.D = D
+        self.E = nn.Linear(D, N * D)
+        self.experts = nn.ModuleList([nn.Linear(D, D) for _ in range(N)])
         self.activation = activation_cls()
-        self.W = nn.Linear(d_model, n_experts)
+        self.W = nn.Linear(D, N)
         self.epsilon = epsilon
-        self.k = k
+        self.K = K
         self.alpha = alpha
 
     def forward(self, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
         # Expert function E: [n] -> R^d -> R^d
         # Weight function G: R^d -> R^n; G(x) = topk(σ(Wx + ε), k)
         # out = Σi G(x)i * Ei(x)
-        batch_size, _ = x.shape
-        assert x.shape == (batch_size, self.d_model), [x.shape, (batch_size, self.d_model)]
-        Ex = self.activation(self.E(x).view(-1, self.n_experts, self.d_model))
-        Exx = torch.cat([self.experts[i](Ex[:, i, :]).unsqueeze(1) for i in range(self.n_experts)], dim=1)
+        B, _ = x.shape
+        assert x.shape == (B, self.D), [x.shape, (B, self.D)]
+        Ex = self.activation(self.E(x).view(-1, self.N, self.D))
+        Exx = torch.cat([self.experts[i](Ex[:, i, :]).unsqueeze(1) for i in range(self.N)], dim=1)
         Wx = self.W(x)
         noise = self.epsilon * torch.randn_like(Wx) if self.training else 0
         Wx_w_noise = Wx + noise
         p = F.softmax(Wx_w_noise, dim=-1)
-        _, topk_indices = torch.topk(p, k=self.k)
+        _, topk_indices = torch.topk(p, k=self.K)
         mask = torch.zeros_like(Wx_w_noise).scatter_(-1, topk_indices, 1)
-        f = torch.sum(mask, dim=0) / batch_size
+        f = torch.sum(mask, dim=0) / B
         Gx_unnormalized = p * mask
         Gx = Gx_unnormalized / torch.sum(Gx_unnormalized, dim=-1, keepdim=True)
         out = torch.einsum("bn,bnd->bd", Gx, Exx)
-        router_loss = self.alpha * self.n_experts * torch.sum(f * p.mean(dim=0))
+        router_loss = self.alpha * self.N * torch.sum(f * p.mean(dim=0))
         return out, router_loss
     
 if __name__ == "__main__":
     torch.manual_seed(42)
-    batch_size = 256
-    d_model = 10
-    n_experts, k = 4, 3
-    moe = MoELayer(n_experts=n_experts, k=k, d_model=d_model)
-    x = torch.randn(batch_size, d_model)
+    B = 256
+    D = 10
+    N, K = 4, 3
+    moe = MoELayer(N=N, K=K, D=D)
+    x = torch.randn(B, D)
     print(moe(x))
--- moe/tests.py
+++ moe/tests.py
@@ -7,54 +7,54 @@ import torch.nn.functional as F
 from moe_naive import MoELayer
 
 class Expert(nn.Module):
-    def __init__(self, d_model: int, activation_cls: type[nn.Module]) -> None:
+    def __init__(self, D: int, activation_cls: type[nn.Module]) -> None:
         super().__init__()
-        self.d_model = d_model
+        self.D = D
         self.activation = activation_cls()
-        self.W1 = nn.Linear(d_model, d_model)
-        self.W2 = nn.Linear(d_model, d_model)
+        self.W1 = nn.Linear(D, D)
+        self.W2 = nn.Linear(D, D)
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        batch_size, _ = x.shape
-        assert x.shape == (batch_size, self.d_model), [x.shape, (batch_size, self.d_model)]
+        B, _ = x.shape
+        assert x.shape == (B, self.D), [x.shape, (B, self.D)]
         return self.W2(self.activation(self.W1(x)))
 
 
 class MoEKernelised(nn.Module):
-    def __init__(self, d_model: int, N: int, K: int, epsilon: float = 1e-5, activation_cls: type[nn.Module] = nn.SiLU) -> None:
+    def __init__(self, D: int, N: int, K: int, epsilon: float = 1e-5, activation_cls: type[nn.Module] = nn.SiLU) -> None:
         super().__init__()
-        self.d_model = d_model
+        self.D = D
         self.N = N
         self.K = K
         self.epsilon = epsilon
-        self.Wg = nn.Linear(d_model, N)
-        self.experts = nn.ModuleList([Expert(d_model, activation_cls) for _ in range(N)])
+        self.Wg = nn.Linear(D, N)
+        self.experts = nn.ModuleList([Expert(D, activation_cls) for _ in range(N)])
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        batch_size, _ = x.shape
-        device = x.device
-        dtype = x.dtype
-        assert x.shape == (batch_size, self.d_model), [x.shape, (batch_size, self.d_model)]
+        B, _ = x.shape
+        assert x.shape == (B, self.D), [x.shape, (B, self.D)]
+        device, dtype = x.device, x.dtype
+        
         Wgx = self.Wg(x)
         noise = self.epsilon * torch.randn_like(Wgx) if self.training else 0
         unnormalised_weights, topk_indices = torch.topk(F.softmax(Wgx + noise, dim=-1), self.K)
         topk_indices = topk_indices.to(torch.int32).contiguous()
         weights = unnormalised_weights / unnormalised_weights.sum(dim=-1, keepdim=True)
-        expert_partitions_to_x = torch.zeros((batch_size * self.K, self.d_model), device=device, dtype=dtype)
-        expert_partitions_to_x_idx = torch.zeros((batch_size * self.K, ), device=device, dtype=torch.int32)
-        expert_partitions_to_weight = torch.zeros((batch_size * self.K,), device=device, dtype=dtype)
+        expert_partitions_to_x = torch.zeros((B * self.K, self.D), device=device, dtype=dtype)
+        expert_partitions_to_x_idx = torch.zeros((B * self.K, ), device=device, dtype=torch.int32)
+        expert_partitions_to_weight = torch.zeros((B * self.K,), device=device, dtype=dtype)
         per_expert_offsets = torch.zeros((self.N + 1, ), device=device, dtype=torch.uint32)
         # compute gate
         moe_kernels.partition_by_expert(topk_indices, x, weights, expert_partitions_to_x, expert_partitions_to_x_idx, expert_partitions_to_weight, per_expert_offsets, self.N)
         # mat mul
-        expert_partitions_to_output = torch.zeros((batch_size * self.K, self.d_model), device=device, dtype=dtype)
+        expert_partitions_to_output = torch.zeros((B * self.K, self.D), device=device, dtype=dtype)
 
         offsets = per_expert_offsets.cpu()
         for i in range(self.N):
             s, t = offsets[i].item(), offsets[i + 1].item()
             if s == t: continue
             expert_partitions_to_output[s:t, :] = self.experts[i](expert_partitions_to_x[s:t,:])
-        out = torch.zeros((batch_size, self.d_model), device=device, dtype=dtype)
+        out = torch.zeros((B, self.D), device=device, dtype=dtype)
 
         moe_kernels.reduce_from_expert_partitions(expert_partitions_to_output, expert_partitions_to_x_idx, expert_partitions_to_weight, out)
         return out
@@ -62,13 +62,13 @@ class MoEKernelised(nn.Module):
 if __name__ == "__main__":
     assert torch.cuda.is_available()
     torch.manual_seed(42)
-    batch_size = 256
-    d_model = 10
-    n_experts, k = 4, 3
+    B = 256
+    D = 10
+    N, K = 4, 3
     with torch.device("cuda"):
-        x = torch.randn(batch_size, d_model)
-        moe = MoELayer(n_experts=n_experts, k=k, d_model=d_model)
-        moe_kernelized = MoEKernelised(d_model=d_model, N=n_experts, K=k).eval()
+        x = torch.randn(B, D)
+        moe = MoELayer(N=N, K=K, D=D)
+        moe_kernelized = MoEKernelised(D=D, N=N, K=K).eval()
         out_ref = moe(x)
         out_kernel = moe_kernelized(x)
     max_abs_err = (out_kernel - out_ref).abs().max().item()
